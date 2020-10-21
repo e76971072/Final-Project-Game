5ea60658804696740a52b8ce1ef35489d972621f
Ian Buchanan-Dempsey <buchanandempsia597@gmail.com>
2020-10-21 12:07:16 -0500

    Added level generation with control over max rooms and creation of end room

---
+++ Assets/Scripts/RoomSpawn.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawn : MonoBehaviour
+{
+    //Used https://www.youtube.com/watch?v=eR74EjkA_4s Blackthornprod as reference for generation
+
+    /*
+    Clockwise starting with top
+    1: Must open bottom
+    2: Must open left
+    3: Must open top
+    4: Must open right
+    */
+    public int openDir;
+    /*  maxRooms
+        0: No Limit
+        5: Minimum max room
+    */
+    public static int maxRooms = 20;
+    public static int totalRooms = 1;
+    private int rand;
+    private RoomTemplates templates;
+    private bool spawned = false;
+
+    public float waitTime = 2f;
+
+    void Start(){
+        Destroy(gameObject, waitTime);
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", .1f);
+    }
+
+    void Spawn(){
+        if(spawned == false)
+        {
+            totalRooms++;
+            //Bottom
+            if(openDir == 1){
+                if(maxRooms == 0 || totalRooms < maxRooms - 4)
+                {
+                    rand = Random.Range(0, templates.bottomRooms.Length);
+                }
+                else
+                {
+                    rand = 0;
+                }
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                
+            //Left
+            }else if(openDir == 2){
+                if(maxRooms == 0 || totalRooms < maxRooms - 4)
+                {
+                    rand = Random.Range(0, templates.leftRooms.Length);
+                }
+                else
+                {
+                    rand = 0;
+                }
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                
+            //Top
+            }else if(openDir == 3){
+                if(maxRooms == 0 || totalRooms < maxRooms - 4)
+                {
+                    rand = Random.Range(0, templates.topRooms.Length);
+                }
+                else
+                {
+                    rand = 0;
+                }
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                
+            //Right
+            }else if(openDir == 4){
+                if(maxRooms == 0 || totalRooms < maxRooms - 4)
+                {
+                    rand = Random.Range(0, templates.rightRooms.Length);
+                }
+                else
+                {
+                    rand = 0;
+                }
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                
+            }
+            spawned = true;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("RoomSpawner"))
+        {
+            if(other.GetComponent<RoomSpawn>().spawned == false && spawned == false){
+                //Wall
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] topRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] bottomRooms;
+    public GameObject[] leftRooms;
+
+    public GameObject closedRoom;
+
+    public List<GameObject> rooms;
+
+    public float bufferTime = 2f;
+    private bool spawnEndRoom = true;
+    public GameObject exit;
+
+    void Update(){
+        if(spawnEndRoom == true && bufferTime <= 0)
+        {
+            for (int i = 0; i < rooms.Count; i++) {
+                if(i == rooms.Count-1){
+                    Instantiate(exit, rooms[i]. transform.position, Quaternion.identity);
+                    spawnEndRoom = false;
+                }
+            }
+        }
+        else {
+            bufferTime -= Time.deltaTime;
+        }
+    }
+}
