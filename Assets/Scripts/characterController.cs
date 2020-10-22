d93359ebb277fb6a32852a58b272b0e7be114b8e
Boss of Siri <an.nguyen1197@gmail.com>
2020-10-22 00:46:40 -0500

    Player movement

---
+++ Assets/Scripts/characterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    Animator animator; 
+         
+
+
+
+
+    private void Start()
+    {
+
+        animator = GetComponent<Animator>(); 
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey("w") || Input.GetKey("d") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("down") || Input.GetKey("up") || Input.GetKey("right") || Input.GetKey("left"))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if ( Input.GetKey ("space"))
+        {
+            animator.SetBool("isAttack", true);
+        }
+
+        if ( ! Input.GetKey("space"))
+        {
+            animator.SetBool("isAttack", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); 
+
+    }
+}
