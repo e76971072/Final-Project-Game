a69547dd438cf7f8e6bbdbe64e002028eee9b021
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2020-10-24 13:06:03 -0500

    Camera system implemented
    Uses cinemachine to swap smoothly between virtual cams added to each room prefab

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject vCam;
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        if(collider.CompareTag("Player") && !collider.isTrigger){
+            vCam.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D collider)
+    {
+        if(collider.CompareTag("Player") && !collider.isTrigger){
+            vCam.SetActive(false);
+        }
+    }
+}
