53f53292c295698c87d955f75fff48e92e5573c8
isbd <buchanandempsia597@gmail.com>
2020-11-27 07:49:27 -0600

    Revert "resolve"
    This reverts commit b9f28a712966e4360fff8455a38bde3105610237, reversing
    changes made to 643073ad667ee90c25d86a219dc11a83c750ed5e.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject camObj;
+    public CinemachineVirtualCamera vCam;
+    private Transform target;
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        if(collider.CompareTag("Player") && !collider.isTrigger){
+            camObj.SetActive(true);
+            target = collider.transform;
+            vCam.Follow = target;
+            vCam.m_Lens.OrthographicSize = 2.75f;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D collider)
+    {
+        if(collider.CompareTag("Player") && !collider.isTrigger){
+            camObj.SetActive(false);
+        }
+    }
+}
