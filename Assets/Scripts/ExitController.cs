ae9e261a3d642b7f4400bb43aed55d1817afc12a
Ian Buchanan-Dempsey <buchanandempsia597@gmail.com>
2020-11-30 11:40:27 -0600

    Added multiple level progression
    Added level progressing, level counting with UI, and centered the health and level UI

---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    [SerializeField] private GameController gameController;
+    public bool check = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!gameController)
+        {
+            gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameController.GetComponent<GameController>().ChangeLevel();
+        }
+    }
+}
